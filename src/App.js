c790a531781a7664b2d41fcf96923f1fc53aa04e
Yeon <jyn990@gmail.com>
2021-11-10 11:10:06 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import LoginForm from "./components/LoginForm"
+// import axios from 'axios'
 
-function App() {
+const App = (props) => {
+  // 로그인 되는 사용자 임시 설정
+  const adminUser = {
+    email: "test@test.com",
+    password: "test123"
+  }
+
+  // 상태 관리
+  const [user, setUser] = useState({email: ""})
+  // 오류 관리
+  const [error, setError] = useState("");
+
+  // 로그인 시도
+  const Login = details => {
+    console.log(details);
+
+    // 로그인 이메일 패스워드 일치 시 로그인
+    if(details.email === adminUser.email && details.password === adminUser.password){
+      console.log("Logged in")
+      setUser({
+        
+        email: details.email
+      })
+      // 불일치 시 오류 메시지
+    } else{
+      console.log("잘못된 비밀번호입니다. 다시 확인하세요.")
+      setError("잘못된 비밀번호입니다. 다시 확인하세요.")
+    }
+  }
+
+  // 로그아웃 -> email 초기화
+  const Logout = () => {
+    console.log("Logout")
+    setUser({
+      email: ""
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    {/* 사용자 이메일이 null이 아니면 환영 페이지 + 로그아웃 버튼 */}
+    {(user.email !== "") ? (
+      <div className="welcome">
+        <h2>Welcome, <span>{user.email}</span></h2>
+        <button onClick={Logout}>Logout</button>
+      </div>
+    ) : (
+      // null이면 에러표시
+      <LoginForm Login={Login} error={error}/>
+    )}
+  </div>
+  )
 }
 
-export default App;
+export default App
